03fe87fadb7ee0facf4663f17b3bc96ac0a513bd
dmah289 <manhnd.ds.gd@gmail.com>
2024-08-04 02:19:27 +0700

    Reapply "Add checkpoint system, trap, effector"
    This reverts commit 2fc82ca151d2dbcdc97a100a65e722cb0efb5a1d.

--- Assets/Scripts/KeySave.cs
+++ Assets/Scripts/KeySave.cs
@@ -6,4 +6,6 @@ public class KeySave : MonoBehaviour
 {
     public static string player = "Player";
     public static string ground = "Ground";
+    public static string obstacle = "Obstacle";
+    public static string whiteScreen = "WhiteScreen";
 }
